0499089676bf9cf2990970580f6492b583afc133
Tarek <tarekmsolli@gmail.com>
2023-04-04 16:51:42 +0100

    added some components

--- PropHub/src/app/app.module.ts
+++ PropHub/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './landing/landing.component';
+import { GameShopComponent } from './game-shop/game-shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    GameShopComponent
   ],
   imports: [
     BrowserModule,
---
+++ PropHub/src/app/game-shop/game-shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-shop',
+  templateUrl: './game-shop.component.html',
+  styleUrls: ['./game-shop.component.css']
+})
+export class GameShopComponent {
+
+}
