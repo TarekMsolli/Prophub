0764585bda898052589b410bd3969863a841754e
Mohamed Ashref Ben Abdallah <mohamedashrefbenabdallah@gmail.com>
2023-04-24 11:12:11 +0100

    raddit el font Poppins : )

--- Prophub/src/app/games/games.component.ts
+++ Prophub/src/app/games/games.component.ts
@@ -4,28 +4,31 @@ import { GameService } from './game.service';
 import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
-  styleUrls: ['./games.component.scss']
+  styleUrls: ['./games.component.scss'],
 })
-export class GamesComponent implements OnInit{
+export class GamesComponent implements OnInit {
   games = [];
   loginId: any;
 
-  private readonly Games_API_URL='http://localhost/games.php'
+  private readonly Games_API_URL = 'http://localhost/games.php';
   term = '';
-  constructor(private GameService: GameService, private loginService:LoginService, private router:Router){}
+  constructor(
+    private GameService: GameService,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    fetch(this.Games_API_URL).then(data => data.json()).then((result)=> this.games = result);
+    fetch(this.Games_API_URL)
+      .then((data) => data.json())
+      .then((result) => (this.games = result));
     this.loginId = this.loginService.getId();
-
   }
 
-  logout(){
+  logout() {
     this.loginService.setId(0);
     this.router.navigate(['/home']);
   }
-
 }
